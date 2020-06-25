d9a484f190539e772cb11abaa509f9d227e2e3fa
jrvillalba1993 <62047235+jrvillalba1993@users.noreply.github.com>
2020-06-25 04:15:48 -0400

    APi added to keys.js

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('keys loaded');
+
+var twitterKeys = {
+  consumer_key:'nQMIMZnKZvMqLthkyIKF7VMR3',
+  consumer_secret:'qZuy1DzFda4t4jkbOG3u8Vdmt6VZMWo57VoiOIb0ML0wkkmqdC',
+  access_token_key:'989581873-Kui3TUKGf96AI7mfpdFDQB3ugBiw7J7WbMxBmckH',
+  access_token_secret:'WCbJVoDKpN0f9X05tnkIR1zDTY3NmnnyTAb5tKKOaQGHS'
+}
+
+module.exports = twitterKeys;
